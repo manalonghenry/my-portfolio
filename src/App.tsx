7c50d173eb700fd13dcf2da73f2877f197c58c06
Mana <arlonghenry@gmail.com>
2025-06-12 16:35:39 -0400

    Add Timeline, Contact, and Footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 // import Test from "./components/testComponent";
-import { Contact } from 'lucide-react';
 import Intro from './components/Intro'
 import Portfolio from './components/Portfolio';
 import Timeline from './components/Timeline';
 import Footer from "./components/Footer";
+import Contact from "./components/Contact";
 
 function App() {
   return (
---
+++ src/data/timeline.ts
@@ -0,0 +1,29 @@
+const timeline =  [
+    {
+        years: '2025',
+        title: "Bachelors's Degree in Computer Science",
+        duration: '5 years',
+        details: 'important stuff',
+    },
+    {
+        years: '2025',
+        title: 'Title 1',
+        duration: 'years',
+        details: 'important stuff',
+    },
+    {
+        years: '2025',
+        title: 'Title 1',
+        duration: 'years',
+        details: 'important stuff',
+    },
+    {
+        years: '2025',
+        title: 'Title 1',
+        duration: 'years',
+        details: 'important stuff',
+    },
+
+];
+
+export default timeline;
