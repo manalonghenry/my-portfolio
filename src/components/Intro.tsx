a8162008c9b06911a9ef8297f1f92b65f8893a38
Mana <arlonghenry@gmail.com>
2025-06-12 17:51:36 -0400

    Add typing animation

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -1,32 +1,45 @@
+import { TypeAnimation } from 'react-type-animation';
+
 function Intro() {
     return (
-        <section className="w-full bg-[#100c1c] text-purple-400">
-          <div className="flex items-center justify-center flex-col text-center min-h-[60vh] pt-20 pb-6 px-4">
-            <h1 className="text-4xl md:text-7xl font-bold font-mono mb-3"> &lt;Amanda Longhenry/&gt;</h1>
-            <p className="text-base md:text-xl mb-3 font-medium">Software Engineer</p>
-            <p className="text-sm max-w-xl mb-6 font-bold">
-              I have a{' '}
-              <a
-                href="https://www.linkedin.com/in/amanda-longhenry/"
-                target="_blank"
-                className="text-pink-300 hover:underline underline-offset-2 decoration-2"
-                rel="noreferrer noopener"
-              >
-                LinkedIn
-              </a>{' '}
-              and a{' '}
-              <a
-                href="https://github.com/manalonghenry"
-                target="_blank"
-                className="text-pink-300 hover:underline underline-offset-2 decoration-2"
-                rel="noreferrer noopener"
-              >
-                GitHub
-              </a>
-            </p>
-          </div>
+        <section className="text-purple-300 pt-20 pb-10 px-4 min-h-[60vh] flex flex-col items-center justify-center bg-[#100c1c]">
+          <TypeAnimation
+            sequence={[
+              '<Amanda Longhenry />',
+              1000,
+            ]}
+            wrapper="h1"
+            speed={60}
+            className="text-4xl md:text-7xl font-bold font-mono mb-3"
+            repeat={0}
+          />
+    
+          <p className="text-base md:text-xl font-medium mb-3">
+            Software Engineer
+          </p>
+    
+          <p className="text-sm max-w-xl mb-6 font-bold">
+            I have a{' '}
+            <a
+              href="https://www.linkedin.com/in/amanda-longhenry/"
+              target="_blank"
+              className="text-pink-200 hover:underline underline-offset-2 decoration-2"
+              rel="noreferrer noopener"
+            >
+              LinkedIn
+            </a>{' '}
+            and a{' '}
+            <a
+              href="https://github.com/manalonghenry"
+              target="_blank"
+              className="text-pink-200 hover:underline underline-offset-2 decoration-2"
+              rel="noreferrer noopener"
+            >
+              GitHub
+            </a>
+          </p>
         </section>
-      );   
+      );  
   }
   
   export default Intro;
