89206f78ade62a400241a42844bf8a3ef02876e8
Mana <arlonghenry@gmail.com>
2025-06-12 17:27:59 -0400

    Purple!
    - New purple name
    - Dark purple background

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,21 @@ import Portfolio from './components/Portfolio';
 import Timeline from './components/Timeline';
 import Footer from "./components/Footer";
 import Contact from "./components/Contact";
+import React, {useEffect, useState} from 'react';
 
 function App() {
+  
   return (
-    <div className="App">
-      <Intro />
-      <Portfolio />
-      <Timeline />
-      <Contact />
-      <Footer />
+    <div className="bg-[#100c1c] text-white min-h-screen font-inter">
+        <div className="max-w-5xl w-11/12 mx-auto">
+          <Intro />
+          <Portfolio />
+          <Timeline />
+          <Contact />
+          <Footer />
+        </div>
     </div>
+    
   );
 }
 
--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -1,30 +1,32 @@
 function Intro() {
     return (
-        <div className="flex items-center justify-center flex-col text-center pt-20 pb-6">
-            <h1 className="text-4xl md:text-7xl mb-1 md:mb-3 font-bold"> Amanda Longhenry</h1>
-            <p className="test-base md:text-xl mb-3 font-medium">Software Engineer</p>
-            <p className="text-sm max-w-xl  mb-6 font-bold"> I have a {' '}
-                <a
-                    href="https://www.linkedin.com/in/amanda-longhenry/"
-                    target="_blank"
-                    className="text-cyan-600 hover:underline underline-offset-2 decoration-2 decoration-pink-300" 
-                    rel="norefferer noopener"
-                >
-                    LinkedIn
-                </a>{' '} 
-                and a {' '}
-                <a
-                    href="https://github.com/manalonghenry"
-                    target="_blank"
-                    className="text-cyan-600 hover:underline underline-offset-2 decoration-2 decoration-pink-300" 
-                    rel="norefferer noopener"
-                >
-                    GitHub
-                </a>
-                </p>
-        </div>
-        
-    )   
+        <section className="w-full bg-[#100c1c] text-purple-400">
+          <div className="flex items-center justify-center flex-col text-center min-h-[60vh] pt-20 pb-6 px-4">
+            <h1 className="text-4xl md:text-7xl font-bold font-mono mb-3"> &lt;Amanda Longhenry/&gt;</h1>
+            <p className="text-base md:text-xl mb-3 font-medium">Software Engineer</p>
+            <p className="text-sm max-w-xl mb-6 font-bold">
+              I have a{' '}
+              <a
+                href="https://www.linkedin.com/in/amanda-longhenry/"
+                target="_blank"
+                className="text-pink-300 hover:underline underline-offset-2 decoration-2"
+                rel="noreferrer noopener"
+              >
+                LinkedIn
+              </a>{' '}
+              and a{' '}
+              <a
+                href="https://github.com/manalonghenry"
+                target="_blank"
+                className="text-pink-300 hover:underline underline-offset-2 decoration-2"
+                rel="noreferrer noopener"
+              >
+                GitHub
+              </a>
+            </p>
+          </div>
+        </section>
+      );   
   }
   
   export default Intro;
